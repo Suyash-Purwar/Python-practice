f76e15565e985bb6c541f0104e13111df4e78346
suyash-purwar <purwarsuyash06@gmail.com>
2022-08-11 13:11:38 +0530

    Push input.py and int213 folder

---
+++ code/basics/input.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+
+name = input("What is your name?\n-> ")
+# The input value through this function is always of type String
+print("Hey", name)
+
+# Type casting functions are used along with the input() function
+age = int(input("My age is: "))
+print(age)
